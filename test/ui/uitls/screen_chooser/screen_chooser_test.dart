5fa91aade0f0e4993b76ecfe2d105f0bf41955db
George Herbert <georgeherby@hotmail.com>
2021-10-24 20:42:23 +0100

    Correct tests

--- test/ui/uitls/screen_chooser/screen_chooser_test.dart
+++ test/ui/uitls/screen_chooser/screen_chooser_test.dart
@@ -1,80 +1,9 @@
-import 'package:crypto_app/core/exceptions/unsupported_device_exceptions.dart';
 import 'package:crypto_app/ui/utils/screen_chooser/screen_chooser.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('DeviceType', () {
-    group('is MobileNotWeb', () {
-      test('if android', () {
-        expect(getDeviceType(TargetPlatform.android, false),
-            equals(DeviceType.mobileNotWeb));
-      });
-      test('if ios', () {
-        expect(getDeviceType(TargetPlatform.android, false),
-            equals(DeviceType.mobileNotWeb));
-      });
-    });
-    group('is MobileWeb', () {
-      test('if android', () {
-        expect(getDeviceType(TargetPlatform.android, true),
-            equals(DeviceType.mobile));
-      });
-      test('if ios', () {
-        expect(getDeviceType(TargetPlatform.android, true),
-            equals(DeviceType.mobile));
-      });
-    });
-    group('is DesktopWeb', () {
-      test('if macOS', () {
-        expect(getDeviceType(TargetPlatform.macOS, true),
-            equals(DeviceType.desktop));
-      });
-      test('if Windows', () {
-        expect(getDeviceType(TargetPlatform.windows, true),
-            equals(DeviceType.desktop));
-      });
-      test('if Linux', () {
-        expect(getDeviceType(TargetPlatform.linux, true),
-            equals(DeviceType.desktop));
-      });
-    });
-    group('is DesktopNotWeb', () {
-      test('if macOS', () {
-        expect(getDeviceType(TargetPlatform.macOS, false),
-            equals(DeviceType.desktopNotWeb));
-      });
-      test('if Windows', () {
-        expect(getDeviceType(TargetPlatform.windows, false),
-            equals(DeviceType.desktopNotWeb));
-      });
-      test('if Linux', () {
-        expect(getDeviceType(TargetPlatform.linux, false),
-            equals(DeviceType.desktopNotWeb));
-      });
-    });
-    group('unsupported', () {
-      group('web', () {
-        test('if Fuchsia', () {
-          expect(
-              () => getDeviceType(TargetPlatform.fuchsia, true),
-              throwsA(predicate((e) =>
-                  e is UnsupportedDevice &&
-                  e.cause == 'TargetPlatform.fuchsia, isWeb true')));
-        });
-      });
-      group('web', () {
-        test('if Fuchsia', () {
-          expect(
-              () => getDeviceType(TargetPlatform.fuchsia, false),
-              throwsA(predicate((e) =>
-                  e is UnsupportedDevice &&
-                  e.cause == 'TargetPlatform.fuchsia, isWeb false')));
-        });
-      });
-    });
-  });
-
+  
   group('ScreenSize', () {
     group('is Mobile', () {
       group('web', () {
