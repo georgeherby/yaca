7cadcde0309ef3382b311a75c95859caccb2da85
George Herbert <github@georgeherby.dev>
2023-01-25 20:57:05 +0000

    Bump versions

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,8 +8,8 @@ import Foundation
 import firebase_analytics
 import firebase_core
 import macos_ui
-import path_provider_macos
-import shared_preferences_macos
+import path_provider_foundation
+import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
